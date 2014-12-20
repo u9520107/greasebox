4275785f7c2db06e76e3cc1e67e01a5f62c76aef
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2014-12-20 18:32:54 +0800

    correct file path

--- test/stylus-transform.js
+++ test/stylus-transform.js
@@ -8,8 +8,8 @@ var cofs = require('co-fs');
 var traceur = require('traceur');
 require(traceur.RUNTIME_PATH);
 
-var stylusTransform = require(path.resolve(__dirname, '../source/node/stylus-transform')).default;
-var rm = require(path.resolve(__dirname, '../dist/node/rm')).default;
+var stylusTransform = require(path.resolve(__dirname, '../source/stylus-transform')).default;
+var rm = require(path.resolve(__dirname, '../dist/rm')).default;
 
 describe('stylusTransform', function() {
   it('should be a function', function () {
