848f3a2780d3157a5079c2aca04b90d9731bde05
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2014-12-29 14:03:35 +0800

    test full traceur compiled build

--- dist/load-map.js
+++ dist/load-map.js
@@ -22,30 +22,98 @@ function loadMap(ext) {
       next();
     } else {
       var self = this;
-      co(function*() {
-        var mapFile = file.path + ext;
-        var map;
-        try {
-          if (yield cofs.exists(mapFile)) {
-            map = JSON.parse(yield cofs.readFile(mapFile));
-          } else {
-            map = {
-              version: 3,
-              file: file.relative,
-              names: [],
-              mappings: '',
-              sources: [file.relative],
-              sourcesContent: [file.contents.toString(enc)],
-              sourceRoot: '/' + path.relative(file.cwd, file.base) + '/'
-            };
-          }
-          file.sourceMap = map;
-          self.push(file);
-          next();
-        } catch (err) {
-          next(err);
-        }
-      });
+      co($traceurRuntime.initGeneratorFunction(function $__4() {
+        var mapFile,
+            map,
+            $__5,
+            $__6,
+            $__7,
+            $__8,
+            $__9,
+            $__10,
+            $__11,
+            $__12,
+            err;
+        return $traceurRuntime.createGeneratorInstance(function($ctx) {
+          while (true)
+            switch ($ctx.state) {
+              case 0:
+                mapFile = file.path + ext;
+                $ctx.state = 30;
+                break;
+              case 30:
+                $ctx.pushTry(20, null);
+                $ctx.state = 23;
+                break;
+              case 23:
+                $__5 = cofs.exists;
+                $__6 = $__5.call(cofs, mapFile);
+                $ctx.state = 6;
+                break;
+              case 6:
+                $ctx.state = 2;
+                return $__6;
+              case 2:
+                $__7 = $ctx.sent;
+                $ctx.state = 4;
+                break;
+              case 4:
+                $ctx.state = ($__7) ? 11 : 15;
+                break;
+              case 11:
+                $__8 = JSON.parse;
+                $__9 = cofs.readFile;
+                $__10 = $__9.call(cofs, mapFile);
+                $ctx.state = 12;
+                break;
+              case 12:
+                $ctx.state = 8;
+                return $__10;
+              case 8:
+                $__11 = $ctx.sent;
+                $ctx.state = 10;
+                break;
+              case 10:
+                $__12 = $__8.call(JSON, $__11);
+                map = $__12;
+                $ctx.state = 14;
+                break;
+              case 15:
+                map = {
+                  version: 3,
+                  file: file.relative,
+                  names: [],
+                  mappings: '',
+                  sources: [file.relative],
+                  sourcesContent: [file.contents.toString(enc)],
+                  sourceRoot: '/' + path.relative(file.cwd, file.base) + '/'
+                };
+                $ctx.state = 14;
+                break;
+              case 14:
+                file.sourceMap = map;
+                self.push(file);
+                next();
+                $ctx.state = 19;
+                break;
+              case 19:
+                $ctx.popTry();
+                $ctx.state = -2;
+                break;
+              case 20:
+                $ctx.popTry();
+                err = $ctx.storedException;
+                $ctx.state = 26;
+                break;
+              case 26:
+                next(err);
+                $ctx.state = -2;
+                break;
+              default:
+                return $ctx.end();
+            }
+        }, $__4, this);
+      }));
     }
   });
 }
